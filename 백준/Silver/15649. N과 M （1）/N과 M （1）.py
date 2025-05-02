ea7a1329c99efeed6523e809c452c483205aedfd
Dongyi Kim <dongyi.dev@gmail.com>
2025-05-02 16:35:57 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,15 @@
+import sys
+
+def back():
+    if len(ans) == m:
+        print(" ".join(map(str, ans)))
+        return
+    for i in range(1, n + 1):
+        if i not in ans:
+            ans.append(i)
+            back()
+            ans.pop()
+
+n,m = map(int, sys.stdin.readline().split(" "))
+ans = []
+back()
